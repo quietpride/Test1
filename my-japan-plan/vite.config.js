0e37bb03817dc859943dcb93b4326407ab6e563a
quietpride <randy.the.professional@gmail.com>
2025-11-25 00:00:55 +0800

    Fix config for Vercel

--- my-japan-plan/vite.config.js
+++ my-japan-plan/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '127.0.0.1', // 強制使用 IPv4
-    port: 5173,        // 強制使用 5173 port
-  }
+  // ⚠️ Vercel 唔需要 base，亦唔需要 server ip 設定
 })
