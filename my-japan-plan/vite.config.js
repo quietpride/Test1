44a2d227b92609ae80650fd8b84b8437c64ca0d4
quietpride <randy.the.professional@gmail.com>
2025-12-21 18:02:33 +0800

    Update itinerary details

--- my-japan-plan/vite.config.js
+++ my-japan-plan/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ⚠️ Vercel 唔需要 base，亦唔需要 server ip 設定
+  base: '/Test1/',  // ⚠️ 這裡一定要填你的 Repository 名，前後要有斜線
 })
